5d00ea4ffb2ed619c02c042531a133f9a1bed679
SamratChakrabortyy <samratch@outlook.com>
2020-07-25 19:09:56 +0530

    Edit Distance

--- src/main/java/in/samratc/main/dynamicProgramming/leetCode/pratice/MaximalRectangle.java
+++ src/main/java/in/samratc/main/dynamicProgramming/leetCode/pratice/MaximalRectangle.java
@@ -1,7 +1,7 @@
 package in.samratc.main.dynamicProgramming.leetCode.pratice;
 
 
-import java.util.Arrays;
+import java.util.Stack;
 import java.util.stream.IntStream;
 
 //https://leetcode.com/problems/maximal-rectangle/
@@ -13,7 +13,7 @@ public class MaximalRectangle {
         int[] heights = new int[n];
         for (char[] arr : matrix) {
             IntStream.range(0, n).forEach(i -> heights[i] = arr[i] == '0' ? 0 : heights[i] + 1);
-            max = Math.max(max, largestHistogramArea(heights));
+            max = Math.max(max, largestRectangleArea(heights));
         }
         return max;
     }
@@ -25,7 +25,7 @@ public class MaximalRectangle {
         rightMin[n - 1] = n;
         for (int i = 1; i < n; i++) {
             int p = i - 1;
-            while ( p >= 0 && heights[p] >= heights[i])
+            while (p >= 0 && heights[p] >= heights[i])
                 p = leftMin[p];
             leftMin[i] = p;
         }
@@ -45,19 +45,54 @@ public class MaximalRectangle {
         return max;
     }
 
+    public int largestRectangleArea(int[] heights) {
+        if(heights == null || heights.length == 0)
+            return 0;
+        int n = heights.length, max = 0;
+        Stack<Pair<Integer, Integer>> stk = new Stack<>();
+        for(int i = 0; i < n; i++){
+            int h = heights[i], start = i;
+            while(!stk.empty() && stk.peek().val2 > h){
+                Pair<Integer, Integer> top = stk.pop();
+                max = Math.max(max, top.val2 * (i - top.val1));
+                start = top.val1;
+            }
+            stk.push(new Pair<Integer, Integer>(start, h));
+        }
+        while(!stk.empty()){
+            Pair<Integer, Integer> top = stk.pop();
+            max = Math.max(max, top.val2 * (n - top.val1));
+        }
+        return max;
+    }
+
     //Driver
     public static void main(String... args) {
 
-        System.out.println(new MaximalRectangle().largestHistogramArea(new int[]{0,0,2,2,2,3,2,1,4,0}));
+        System.out.println(new MaximalRectangle().largestHistogramArea(new int[]{0, 0, 2, 2, 2, 3, 2, 1, 4, 0}));
         char[][] arr = {{'0', '1', '1', '0', '0', '1', '0', '1', '0', '1'},
-                        {'0', '0', '1', '0', '1', '0', '1', '0', '1', '0'},
-                        {'1', '0', '0', '0', '0', '1', '0', '1', '1', '0'},
-                        {'0', '1', '1', '1', '1', '1', '1', '0', '1', '0'},
-                        {'0', '0', '1', '1', '1', '1', '1', '1', '1', '0'},
-                        {'1', '1', '0', '1', '0', '1', '1', '1', '1', '0'},
-                        {'0', '0', '0', '1', '1', '0', '0', '0', '1', '0'},
-                        {'1', '1', '0', '1', '1', '0', '0', '1', '1', '1'},
-                        {'0', '1', '0', '1', '1', '0', '1', '0', '1', '1'}};
+                {'0', '0', '1', '0', '1', '0', '1', '0', '1', '0'},
+                {'1', '0', '0', '0', '0', '1', '0', '1', '1', '0'},
+                {'0', '1', '1', '1', '1', '1', '1', '0', '1', '0'},
+                {'0', '0', '1', '1', '1', '1', '1', '1', '1', '0'},
+                {'1', '1', '0', '1', '0', '1', '1', '1', '1', '0'},
+                {'0', '0', '0', '1', '1', '0', '0', '0', '1', '0'},
+                {'1', '1', '0', '1', '1', '0', '0', '1', '1', '1'},
+                {'0', '1', '0', '1', '1', '0', '1', '0', '1', '1'}};
         System.out.println(new MaximalRectangle().maximalRectangle(arr));
+    }
+}
+
+class Pair<E,F>{
+    public E val1;
+    public F val2;
+
+    public Pair(E e, F f){
+        this.val1 = e;
+        this.val2 = f;
+    }
+
+    public String toString(){
+        return String.format("(%s, %s)", this.val1, this.val2);
     }
 }
