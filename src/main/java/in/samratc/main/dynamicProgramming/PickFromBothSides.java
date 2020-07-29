50e21667d554a098b11a5e3b7b77d41c89b77b49
SamratChakrabortyy <samratch@outlook.com>
2020-07-30 03:32:50 +0530

    Pick up from Bothends

---
+++ src/main/java/in/samratc/main/dynamicProgramming/PickFromBothSides.java
@@ -0,0 +1,52 @@
+package in.samratc.main.dynamicProgramming;
+
+public class PickFromBothSides {
+    /**
+     * Given an integer array nums of size N.
+     * You can pick k elements from either left or right end of the array nums to get maximum sum.
+     * Find and return this maximum possible sum.
+     * <p>
+     * NOTE: Suppose k = 4 and array nums contains 10 elements then:
+     * You can pick first four elements or can pick last four elements or can pick 1 from front and 3 from back etc .
+     * You need to return the maximum possible sum of elements you can pick.
+     */
+    public int solve(int[] nums, int k) {
+        /*
+                At first it looks like a Greedy problem that we can just pick the largest ones and sum them.
+                But think of the example nums = {7, 5, 4, 4 100, -1} and k = 2 Greedy will lead to an answer of 12 (7 + 5) but the answer is 99 (100 + (-1))
+
+                The second thing after greedy which comes to mid is DP which can be easily formed by the recursive eq :
+                        maxSum(num, 0, N, k) = max((nums[0] + maxSum(nums, 1, N, k-1)), (nums[N] + maxSum(nums, 0, N - 1, k-1));
+
+                 Though it looks like a 3D DP but if you look closely it is not as start, end and length are interdependent variables
+                 i.e for start = i and len = k - x it is evident that we have already chosen i-1 elements form left and x-i-1 elements from right
+                 Therefore end = N - (x - i - 1)
+
+                 So, its evidently a 2D DP so the no. of states = N^2
+
+                 But if you look closely you'll find out that we can solve this O(k) for both time and space.
+                 See the code ;)  (PS this is discussed not my own, LOL)
+         */
+        if (nums == null || nums.length == 0 || k <= 0)
+            return 0;
+        int n = nums.length;
+        int[] prefixSum = new int[k + 1];
+        prefixSum[0] = 0;
+        for (int i = 1; i <= k; i++) {
+            prefixSum[i] += prefixSum[i - 1] + nums[i-1];
+        }
+        int max = prefixSum[k], suffixSum = 0;
+        for (int i = 0; i < k; i++) {
+            suffixSum += nums[n-1-i];
+            max = Math.max(max, suffixSum + prefixSum[k-1-i]);
+        }
+        return max;
+    }
+
+    //Driver
+    public static void main(String... args){
+        PickFromBothSides bs = new PickFromBothSides();
+        System.out.println(bs.solve(new int[]{7, 5, 4, 4, 100, -1}, 1));
+        System.out.println(bs.solve(new int[]{7, 5, 4, 4, 100, -1}, 2));
+    }
+}
--- src/main/java/in/samratc/main/tree/RemoveExtraEdge.java
+++ src/main/java/in/samratc/main/tree/RemoveExtraEdge.java
@@ -1,16 +1,54 @@
 package in.samratc.main.tree;
 
+
+import java.util.*;
+
 public class RemoveExtraEdge {
     /**
      * Remove Extra Edge
      * Problem Description
-     *
+     * <p>
      * Given a binary tree A where all values of nodes are distinct, there is a pair of leaf nodes which have the same value.
-     *
+     * <p>
      * Considering that the pair that has the same value is indeed the same node and not two different nodes, return the tree after removing one of the edges from it.
-     *
+     * <p>
      * You need to remove the edge from the parent whose value is greater.
      */
 
+    Map<Integer, TreeNode> leafNodeParent;
+    public TreeNode solve(TreeNode root) {
+        leafNodeParent = new HashMap<>();
+        removeDuplicateEdge(root, null);
+        return root;
+    }
+    private void removeDuplicateEdge(TreeNode node, TreeNode parent){
+        if(node.left == null && node.right == null){
+            if(!leafNodeParent.containsKey(node.val)){
+                leafNodeParent.put(node.val, parent);
+            } else{
+                TreeNode largerParent = parent.val > leafNodeParent.get(node.val).val ? parent : leafNodeParent.get(node.val);
+                if(largerParent.left != null && largerParent.left.val == node.val)
+                    largerParent.left = null;
+                else
+                    largerParent.right = null;
+            }
+        }
+        if(node.left != null)
+            removeDuplicateEdge(node.left, node);
+        if(node.right != null)
+            removeDuplicateEdge(node.right, node);
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+            left = null;
+            right = null;
+        }
+    }
 
 }
